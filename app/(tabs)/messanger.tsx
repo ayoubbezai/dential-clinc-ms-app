79856c73fdccf4672408b9965f0e802dc30bdfcf
ayoubbezai <bezaiayoub8@gmail.com>
2025-05-05 02:07:18 +0100

    fix messages

--- app/(tabs)/messanger.tsx
+++ app/(tabs)/messanger.tsx
@@ -4,19 +4,19 @@ import { useAuth } from "@/context/AuthContext";
 import { Text, View } from "react-native";
 
 const Messenger = () => {
-  const { token } = useAuth();
+  const { token, user } = useAuth();
 
-  const patientId = "01962eb2-54c7-71b9-9de4-64316cdae92b"; // Hardcoded for now
+  const patientId = user?.id;
 
   useEffect(() => {
     if (!token || !patientId) return;
 
     const pusher = new Pusher("xs5n6ysk7wwrglkxyrle", {
-      wsHost: "192.168.1.2",
-      wsPort: 6001,
+      wsHost: "192.168.1.8",
+      wsPort: 8080,
       forceTLS: false,
       enabledTransports: ["ws", "wss"],
-      authEndpoint: "http://192.168.1.2:8000/api/broadcasting/auth",
+      authEndpoint: "http://192.168.1.8:8000/api/broadcasting/auth",
       cluster: "",
       auth: {
         headers: {
@@ -28,13 +28,12 @@ const Messenger = () => {
     const channel = pusher.subscribe(`private-chat.patient.${patientId}`);
 
     channel.bind("pusher:subscription_succeeded", () => {
-      console.log(`✅ Subscribed to private-chat.patient.${patientId}`);
+      console.log(`✅ Subscribed to private-chat.${patientId}`);
     });
 
     channel.bind("pusher:subscription_error", (err: any) => {
       console.error("❌ Subscription error:", err);
     });
-
     channel.bind("message.sent", (data: any) => {
       console.log("📩 New message received:", data);
 
